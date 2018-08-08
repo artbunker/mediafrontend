ec29dcce728b0e26235fb41c4fb8e470a122d801
secret <secretisdead@gmail.com>
2018-08-07 17:04:20 -0700

    add timed automatic slideshow navigation

--- mediaarchive/views/static/scripts/view.js
+++ mediaarchive/views/static/scripts/view.js
@@ -27,3 +27,38 @@ if (blacklisted_tags) {
 		}
 	}
 }
+// auto slideshow
+let slideshow_delay_s = localStorage.getItem('slideshow_delay_s');
+let medium_prev = document.querySelector('#medium_prev');
+let medium_next = document.querySelector('#medium_next');
+if (slideshow_delay_s) {
+	if ('backward' != localStorage.getItem('slideshow_direction')) {
+		if (medium_next) {
+			setTimeout(() => {
+				window.location = (medium_next.href);
+			}, slideshow_delay_s * 1000);
+		}
+	}
+	else if (medium_prev) {
+		setTimeout(() => {
+			window.location = (medium_prev.href);
+		}, slideshow_delay_s * 1000);
+	}
+}
+// slideshow navigation direction
+if (medium_prev) {
+	medium_prev.addEventListener('navigate', e => {
+		localStorage.setItem('slideshow_direction', 'backward');
+	});
+	medium_prev.addEventListener('click', e => {
+		localStorage.setItem('slideshow_direction', 'backward');
+	});
+}
+if (medium_next) {
+	medium_next.addEventListener('navigate', e => {
+		localStorage.setItem('slideshow_direction', 'forward');
+	});
+	medium_next.addEventListener('click', e => {
+		localStorage.setItem('slideshow_direction', 'forward');
+	});
+}
